a2498ad837c9c72f0196490bae3bec6df78c2ea2
ShivanathBabu <shivanathbabu.kodimyala@gmail.com>
2025-07-16 22:38:32 +0530

    terraform

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id  # fetches public subnet id
 }
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
